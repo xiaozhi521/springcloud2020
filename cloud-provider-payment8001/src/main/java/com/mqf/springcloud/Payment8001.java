3c029e6d87841313830a05085f6904060afd2128
lihongzhi <1160948933@qq.com>
2020-03-24 00:05:40 +0800

    payment8001 注册到eureka

--- cloud-provider-payment8001/src/main/java/com/mqf/springcloud/Payment8001.java
+++ cloud-provider-payment8001/src/main/java/com/mqf/springcloud/Payment8001.java
@@ -2,11 +2,13 @@ package com.mqf.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @create 2020-03-21 10:18
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class Payment8001 {
     public static void main(String[] args) {
         SpringApplication.run(Payment8001.class,args);
