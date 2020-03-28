5e31de85d7a8fffe209f2b178c3d06e6f6eb394a
lihongzhi <1160948933@qq.com>
2020-03-28 15:53:18 +0800

    单机order80 注册到eureka

--- cloud-consumer-order80/src/main/java/com/mqf/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/mqf/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.mqf.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author mu_qingfeng
  * @date 2020/3/22 3:38 PM
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
