b6deb7b95f71a671e377899d5503117ad011ce42
lihongzhi <1160948933@qq.com>
2020-03-29 20:02:59 +0800

    consul 整合服务提供者

---
+++ cloud-provider-payment8006/src/main/java/com/mqf/springcloud/PaymentMain8006.java
@@ -0,0 +1,17 @@
+package com.mqf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author mu_qingfeng
+ * @date 2020/3/29 7:55 PM
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8006 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8006.class,args);
+    }
+}
