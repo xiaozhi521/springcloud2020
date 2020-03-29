93e410847bc4c66e8b7e2f5f02579ac7ddf9c0ae
lihongzhi <1160948933@qq.com>
2020-03-29 19:18:29 +0800

    zookeeper 整合服务消费者

---
+++ cloud-consumerzk-order80/src/main/java/com/mqf/springcloud/OrderMainZK80.java
@@ -0,0 +1,15 @@
+package com.mqf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mu_qingfeng
+ * @date 2020/3/29 6:01 PM
+ */
+@SpringBootApplication
+public class OrderMainZK80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMainZK80.class,args);
+    }
+}
