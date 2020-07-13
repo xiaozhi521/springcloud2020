8401f456fd1c1eeaf9fb06f82ca2ff7822dc81f6
lihongzhi <1160948933@qq.com>
2020-07-13 09:06:26 +0800

    add dashboard监控（64）

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/mqf/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.mqf.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author mu_qingfeng
+ * @date 2020/07/11 15:58 PM
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
--- cloud-provider-payment8001/src/main/java/com/mqf/springcloud/Payment8001.java
+++ cloud-provider-payment8001/src/main/java/com/mqf/springcloud/Payment8001.java
@@ -14,8 +14,9 @@ import org.springframework.context.annotation.Bean;
  * @create 2020-03-21 10:18
  */
 @SpringBootApplication
-@EnableEurekaClient
-@EnableCircuitBreaker
+@EnableEurekaClient //本服务启动后自动会自动注册进 eureka 服务中
+@EnableDiscoveryClient  //服务发现
+@EnableCircuitBreaker   //对hystrixR熔断机制的支持
 public class Payment8001 {
     public static void main(String[] args) {
         SpringApplication.run(Payment8001.class,args);
