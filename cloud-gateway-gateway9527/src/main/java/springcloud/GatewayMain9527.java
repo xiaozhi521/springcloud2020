9b28dd5d280d870418e67920d8b65177da76dc62
lihongzhi <1160948933@qq.com>
2020-07-16 19:44:44 +0800

    add gateway服务网关（70）增加Bean注入

--- cloud-gateway-gateway9527/src/main/java/springcloud/GatewayMain9527.java
+++ cloud-gateway-gateway9527/src/main/java/springcloud/GatewayMain9527.java
@@ -2,7 +2,12 @@ package springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import static org.springframework.cloud.gateway.support.RouteMetadataUtils.RESPONSE_TIMEOUT_ATTR;
 
 /**
  * @create 2020-07-15 22:13
@@ -12,5 +17,25 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class GatewayMain9527 {
     public static void main(String[] args) {
         SpringApplication.run(GatewayMain9527.class,args);
+    }
+
+
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder routeBuilder){
+//        RouteLocatorBuilder.Builder routes = routeBuilder.routes();
+//        routes.route("path_route_mqf", r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+//        return routes.build();
+//    }
+
+
+    /**
+     *  当访问 localhost:9527/guoji 时会自动转发到  http://news.baidu.com/guoji
+     * @param routeBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeBuilder){
+        return routeBuilder.routes()
+                .route("test1", r -> r.path("/guoji").uri("http://news.baidu.com/guoji")).build();
     }
 }
